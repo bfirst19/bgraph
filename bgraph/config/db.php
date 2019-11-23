a97247f110319965a33532be77b0d3e3f13e2a96
bfirst19 <poetalien@outlook.com>
2019-11-23 19:43:34 +0530

    deleted the files for new location

--- bgraph/config/db.php
+++
@@ -1,8 +0,0 @@
-
-<?php
-$con = mysqli_connect("localhost","bgraph","Bgraph2019*","badm");
-if (mysqli_connect_errno())
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
-?>
--- bgraph/doc/dashboard.php
+++
@@ -1,6 +0,0 @@
-<?php include 'header.php'; ?>
-
-
-<h1>This is dashboard window for status</h1>
-
-<?php include 'footer.php'; ?>
